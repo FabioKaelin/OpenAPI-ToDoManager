475f2e1f0ee3fa05f688bd30ff24d3a7713f6f68
FabioKaelin <90179796+FabioKaelin@users.noreply.github.com>
2022-01-12 13:28:22 +0000

    DB einbindungsModul

---
+++ src/utils/db.js
@@ -0,0 +1,40 @@
+const logger = require('./logger');
+
+const initOptions = {
+  query(e) {
+    logger.log('info', e.query);
+  },
+  error: (err, e) => {
+    // The error applies to the result from the following methods: none, one, oneOrNone and many.
+    if (err instanceof pgp.errors.QueryResultError) {
+      logger.error('%s: %s. Query: %s.', err.name, err.message, err.query);
+    }
+  },
+};
+
+const pgp = require('pg-promise')(initOptions);
+
+const cn = {
+  host: '172.19.0.3',
+  port: 5432,
+  database: 'm295',
+  user: 'admin',
+  password: 'secret-pwd',
+};
+
+// creates the object, but it does not try to connect
+const db = pgp(/* process.env.DATABASE_CONNECTOR */ cn);
+
+// Test the connection and log Postgres server version
+// if successful; or else log connection error
+db.connect()
+  .then((obj) => {
+    const serverVersion = obj.client.serverVersion;
+    logger.log('info', 'Database server version: ' + serverVersion);
+    obj.done(); // success, release the connection;
+  })
+  .catch((error) => {
+    logger.log('error', error.message || error);
+  });
+
+module.exports = db;
