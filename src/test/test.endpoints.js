ca6761a049ce57649bf2c81e840bd69278601210
FabioKaelin <90179796+FabioKaelin@users.noreply.github.com>
2022-01-12 10:43:02 +0000

    modified

--- src/test/test.endpoints.js
+++ src/test/test.endpoints.js
@@ -1,11 +1,10 @@
 var assert = require('assert');
-var request = require("supertest");
+var request = require('supertest');
 var app = require('../api');
 
-describe('API Endpoints', function() {
-
-  describe('GET /v1/ping', function() {
-    it('responds with JSON', function(done) {
+describe('API Endpoints', function () {
+  describe('GET /v1/ping', function () {
+    it('responds with JSON', function (done) {
       request(app)
         .get('/v1/ping')
         .set('Accept', 'application/json')
@@ -16,5 +15,18 @@ describe('API Endpoints', function() {
     // });
     // it('fails if parameter contain reserved characters like !', function(done) {
     // });
-  })
+  });
+  describe('GET /v1/healthcheck', function () {
+    it('responds with JSON', function (done) {
+      request(app)
+        .get('/v1/healthcheck')
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /json/)
+        .expect(200, done);
+    });
+    // it('responds with given parameter', function(done) {
+    // });
+    // it('fails if parameter contain reserved characters like !', function(done) {
+    // });
+  });
 });
