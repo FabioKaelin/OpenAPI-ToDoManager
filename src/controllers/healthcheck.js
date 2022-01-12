e6303d22a9cc40081d503fda727b0be02b09fa6f
FabioKaelin <90179796+FabioKaelin@users.noreply.github.com>
2022-01-12 10:42:48 +0000

    Modified

--- src/controllers/healthcheck.js
+++ src/controllers/healthcheck.js
@@ -2,8 +2,8 @@ const { APIResponse } = require('../utils/response');
 
 function healthcheck(req, res) {
   const data = {
-    date: new Date(),
     betriebszeit: process.uptime(),
+    Zeitstempel: new Date(),
   };
   const healthcheck = new APIResponse(200, "I'm available, what's up?", data);
   res.status(healthcheck.code).json(healthcheck);
