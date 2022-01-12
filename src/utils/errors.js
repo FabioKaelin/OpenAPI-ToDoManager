f274a3df2c7ee54a739fe6a6b699ba31b5f5eacf
FabioKaelin <90179796+FabioKaelin@users.noreply.github.com>
2022-01-12 13:29:32 +0000

    Unauthorized added

--- src/utils/errors.js
+++ src/utils/errors.js
@@ -8,6 +8,9 @@ class GeneralError extends Error {
     if (this instanceof NotFound) {
       return 404;
     }
+    if (this instanceof Unauthorized) {
+      return 401;
+    }
     if (this instanceof BadRequest) {
       return 400;
     }
@@ -18,18 +21,23 @@ class GeneralError extends Error {
     if (this instanceof NotFound) {
       return "I didn't found what you requested.";
     }
+    if (this instanceof Unauthorized) {
+      return 'You are Unautorized.';
+    }
     if (this instanceof BadRequest) {
       return "I can't process your request.";
     }
-    return "I will not answer your request ";
+    return 'I will not answer your request ';
   }
 }
 
-class NotFound extends GeneralError { }
-class BadRequest extends GeneralError { }
+class NotFound extends GeneralError {}
+class BadRequest extends GeneralError {}
+class Unauthorized extends GeneralError {}
 
 module.exports = {
   GeneralError,
   NotFound,
-  BadRequest
+  BadRequest,
+  Unauthorized,
 };
